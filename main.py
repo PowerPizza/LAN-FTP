28be80b9cb93408c6c01e0f0d3f52e1672305a3e
scihack223 <goatbale@gmail.com>
2023-03-27 22:14:47 +0530

    Version 1

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import json, os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+writeIN = None
+@app.route("/createFile", methods=["POST"])
+def createFile():
+    global writeIN
+    meta_data_ = json.loads(request.data.decode())
+    with open(f"currentFiles/{meta_data_['name']}", "wb") as fp:
+        fp.close()
+    writeIN = meta_data_['name']
+    return "OK"
+
+@app.route("/writeFile", methods=["POST"])
+def writeFile():
+    global writeIN
+    with open(f"currentFiles/{writeIN}", "wb") as fp:
+        fp.write(request.data)
+    writeIN = None
+    return "OK"
+
+
+@app.route("/listDownloadable", methods=["POST"])
+def list_Downloadable():
+    to_ret = {"files": os.listdir("currentFiles/")}
+    return json.dumps(to_ret)
+
+@app.route("/downloadFile", methods=["POST"])
+def downloadFile():
+    file_info = json.loads(request.data.decode())
+    with open(f"currentFiles/{file_info['name']}", "rb") as fp:
+        return fp.read()
+
+app.run(host="0.0.0.0", port=8080)
---
+++ readme.md
@@ -0,0 +1,8 @@
+This program help us to transfer file from on device to other devices.
+
+---
+__HOW TO USE :-__
+-
+- First run main.py on your pc or on your android device, just remember that the device on which this file will run act as server.
+- After running the file you will get the IP of webserver in output console just share it with all the clients of network.
+- Open IP:PORT in browser to access the GUI webserver.
